95537128e8a8b2f0853d801dfacaac4f3a9b46a5
Pedro <pedro@staterra.com.br>
2024-03-21 17:13:11 -0300

    Patched dataset and removed NaN values

--- data/clear_data.py
+++ data/clear_data.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 cancer_df = pd.read_csv(r"C:\Users\pedro\Documents\WineQualityPredictor\data\cancer_reg.csv", encoding='latin-1')
-dups = list(cancer_df.duplicated())
-
-indexes = [i for i, x in enumerate(dups) if x]
-cancer_df = cancer_df.drop(index=indexes)
+cancer_df = cancer_df.dropna()
 
 cancer_df.to_csv("cancer_reg_cleaned.csv")
